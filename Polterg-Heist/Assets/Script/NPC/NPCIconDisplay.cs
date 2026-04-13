e88fe36944508f4afd18d7b804ac23724a06ffb9
Lilou <lilou.blanchette@hotmail.com>
2026-04-12 20:24:48 -0400

    Comment NPCIconDisplay

--- Polterg-Heist/Assets/Script/NPC/NPCIconDisplay.cs
+++ Polterg-Heist/Assets/Script/NPC/NPCIconDisplay.cs
@@ -4,23 +4,26 @@ using UnityEngine.Rendering.Universal;
 public class NPCIconDisplay : MonoBehaviour
 {
 
-    [SerializeField] private SpriteRenderer iconRenderer;
-    private Light2D fovLight;
+    [SerializeField] private SpriteRenderer iconRenderer;       // Renderer used to display the icon above the NPC
+    private Light2D fovLight;                                   // Light reprensenting the FOV of the NPC
 
-    [SerializeField] protected Sprite investigationIcon;
-    [SerializeField] protected Sprite alertIcon;
+    [Header("Icons")]
+    [SerializeField] protected Sprite investigationIcon;        // Icon displayed when NPC is investigating
+    [SerializeField] protected Sprite alertIcon;                // Icon displayed when NPC is alerted
 
-    [SerializeField] protected Color nonSuspiciousColorFOV;
-    [SerializeField] protected Color alertColorFOV;
+    [Header("FOV Colors")]
+    [SerializeField] protected Color nonSuspiciousColorFOV;     // Default FOV color when NPC is non suspicious
+    [SerializeField] protected Color alertColorFOV;             // FOV color when NPC is alerted
 
     private void Awake()
     {
         fovLight = GetComponentInChildren<Light2D>();
     }
 
+    // Change the icon and the FOV light color base on the given state.
     public void UpdateIcon(IconState state)
     {
-        if (iconRenderer == null) return;
+        if (iconRenderer == null || fovLight == null) return;
 
         switch (state)
         {
@@ -41,6 +44,7 @@ public class NPCIconDisplay : MonoBehaviour
         }
     }
 
+    // Resets the icon display and the FOV light to its default state.
     public void Reset()
     {
         if (iconRenderer != null)
